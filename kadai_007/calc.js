8de0928757d34e82f6855e49dd36d2b33f34758c
yu-ki2249 <watabeyuki04092002@gmail.com>
2024-02-11 13:58:05 +0900

    kadai-_007を追加

---
+++ kadai_007/calc.js
@@ -0,0 +1,24 @@
+let num =30;
+
+//確認用
+console.log(num);
+
+//変数numが３の倍数の場合
+if (num % 3=== 0 ) {
+    console.log('3の倍数です');
+}
+
+//変数numが5の倍数の場合
+ if (num % 5 === 0) {
+    console.log('5の倍数です');
+}
+
+//変数numが3と5の倍数の場合
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log('3と5の倍数です');
+}
+
+// それ以外の場合
+else  {
+    console.log(num);
+}
