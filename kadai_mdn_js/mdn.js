b58337d3ead65231dfcecd6d8b806a99b3fe57f6
yu-ki2249 <watabeyuki04092002@gmail.com>
2024-02-12 18:48:40 +0900

    kadai_mdn_jsを追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+// 関数
+const yearMonthDate = new Date();
+const year = yearMonthDate.getFullYear();
+const month = yearMonthDate.getMonth();
+const date = yearMonthDate.getDate();
+   
+// コンソールで出力
+console.log(year + '年', month + '月', date + '日');
