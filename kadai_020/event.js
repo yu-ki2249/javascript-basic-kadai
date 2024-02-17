f7207c3771b899aac89a62dc37a9c1fc85f21d8f
yu-ki2249 <watabeyuki04092002@gmail.com>
2024-02-18 00:53:16 +0900

    kadai_020を追加

---
+++ kadai_020/event.js
@@ -0,0 +1,11 @@
+// textというidを持つHTML要素を取得し、定数に代入する
+const textMessage = document.getElementById('text');
+
+// btnというidを持つHTML要素を取得し、定数に代入する
+const Btn  = document.getElementById('btn');
+
+/*ボタンをクリックした後に「ボタンをクリックしてください」
+表示から「ボタンをクリックしましたに表示を変える*/
+Btn.addEventListener('click', () => { 
+  textMessage.textContent = 'ボタンをクリックしました'
+});
